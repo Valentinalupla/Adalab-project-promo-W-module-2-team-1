bbae77e7e9e54ce8a5e32b69eb0d698a8d3e2430
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-02-19 13:16:55 +0100

    Javascript Share

--- src/js/share.js
+++ src/js/share.js
@@ -3,14 +3,33 @@
 const profileCreated = document.querySelector ('.js-profile'); 
 const linkButton = document.querySelector ('.js-button');
 
+const hidenMenu = document.querySelector(".js-collapsedBotton");
+const iconUp = document.querySelector(".js-up");
+const iconDown = document.querySelector(".js-down");
+
 function handleClickProfile(event) {
     event.preventDefault();
 
     linkButton.classList.add ('play');
     profileCreated.classList.remove ('hiden');
-    
-  }
+}
 
-  linkButton.addEventListener('click', handleClickProfile);
+linkButton.addEventListener('click', handleClickProfile);
 
+  
+iconUp.addEventListener("click", () => {
+  hiden();
+});
+iconDown.addEventListener("click", () => {
+  console.log('click')
+  hiden();
 
+});
+
+function hiden() {
+
+  iconUp.classList.toggle("hiden");
+  iconDown.classList.toggle("hiden");
+  hidenMenu.classList.toggle("hiden");
+  profileCreated.classList.toggle ("hiden")
+}
