195ccffab4ce0f583f88c52023bfc4f24ed69fdc
mloherr <herraizmaria08@gmail.com>
2024-02-21 09:56:41 +0100

    js refactored code

--- src/js/mainSectionImage.js
+++ src/js/mainSectionImage.js
@@ -9,20 +9,32 @@ const defaultJobCard = document.querySelector('.js-preview-JobCard');
 
 const resetButton = document.querySelector('.js-button-reset');
 
-const handleReset = () => {
+const resetCLassIcon = () => {
   selectedIcons.forEach((icon) => {
     icon.classList.remove('changereds', 'changeblues', 'changeneutrals');
   });
   cardText.classList.remove('changereds', 'changeblues', 'changeneutrals');
   cardDecoration.classList.remove('decoR', 'decoB', 'decoN');
+};
+
+const resetRadioButtons = () => {
   document.querySelectorAll('.inputpal').forEach((radioButton) => {
     radioButton.checked = false;
   });
+};
+
+const resetTextCard = () => {
   for (var i = 0; i < formInformationUser.length; i++) {
     formInformationUser[i].value = formInformationUser[i].defaultValue;
   }
   defaultNameCard.innerHTML = 'Nombre Apellido';
   defaultJobCard.innerHTML = 'Front-end developer';
+};
+
+const handleReset = () => {
+  resetCLassIcon();
+  resetRadioButtons();
+  resetTextCard();
 };
 
 resetButton.addEventListener('click', handleReset);
