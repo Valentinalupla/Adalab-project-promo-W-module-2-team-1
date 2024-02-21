a535f03482725e75bec8d199a249e00cdddfed21
mloherr <herraizmaria08@gmail.com>
2024-02-21 08:28:52 +0100

    reset form - done half

--- src/js/mainSectionImage.js
+++ src/js/mainSectionImage.js
@@ -3,6 +3,7 @@
 const selectedIcons = document.querySelectorAll('.js-icons');
 const cardDecoration = document.querySelector('.js-decorationCard');
 const cardText = document.querySelector('.js-headerCard');
+const formInformationUser = document.querySelectorAll('input');
 
 const resetButton = document.querySelector('.js-button-reset');
 
@@ -12,6 +13,12 @@ const handleReset = () => {
   });
   cardText.classList.remove('changereds', 'changeblues', 'changeneutrals');
   cardDecoration.classList.remove('decoR', 'decoB', 'decoN');
+  document.querySelectorAll('.inputpal').forEach((radioButton) => {
+    radioButton.checked = false;
+  });
+  for (var i = 0; i < formInformationUser.length; i++) {
+    formInformationUser[i].value = formInformationUser[i].defaultValue;
+  }
 };
 
 resetButton.addEventListener('click', handleReset);
