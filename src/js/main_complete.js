f73dd16a981fe0b4ec028f3949a269eb1eb98831
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-02-21 13:59:14 +0100

    image preview

--- src/js/main_complete.js
+++ src/js/main_complete.js
@@ -12,8 +12,37 @@ const inputGit = document.querySelector('.js-git');
 const cardPreviewName = document.querySelector('.js-preview-NameCard');
 const cardPreviewJob = document.querySelector('.js-preview-JobCard');
 
+//código img
+const fr = new FileReader();
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
 
 
+/**
+
+ * @param {evento} e 
+ */
+function getImage(e){
+  const myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+}
+
+
+
+function writeImage() {
+
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+
+
+
+fileField.addEventListener('change', getImage);
+
+
+//código Valentina
 
 function liveEvent() {
   cardPreviewName.innerHTML = inputName.value; 
@@ -21,16 +50,7 @@ function liveEvent() {
 }
 
 inputName.addEventListener('input', liveEvent);
-
-
-
-
-
-
-
-
-
-
+inputJob.addEventListener('input', liveEvent);
 
 
 function hiddenMenu() {
