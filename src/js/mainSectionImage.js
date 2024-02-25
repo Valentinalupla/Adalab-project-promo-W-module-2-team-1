48cc0dc722ea5ca7babf33ffff2018dc63992e28
mloherr <herraizmaria08@gmail.com>
2024-02-25 10:51:51 +0100

    added new functions reset

--- src/js/mainSectionImage.js
+++ src/js/mainSectionImage.js
@@ -9,7 +9,12 @@ const defaultNameCard = document.querySelector('.js-preview-NameCard');
 const defaultJobCard = document.querySelector('.js-preview-JobCard');
 const resetButton = document.querySelector('.js-button-reset');
 const uploadProfileImage = document.querySelector('.js__profile-image');
+const inputEmail = document.querySelector('.js-email');
+const inputNumber = document.querySelector('.js-number');
+const inputLinkedin = document.querySelector('.js-linkedin');
+const inputGit = document.querySelector('.js-git');
 
+// Resetear tarjeta
 const resetCLassIcon = () => {
   selectedIcons.forEach((icon) => {
     icon.classList.remove('changereds', 'changeblues', 'changeneutrals');
@@ -18,29 +23,64 @@ const resetCLassIcon = () => {
   cardDecoration.classList.remove('decoR', 'decoB', 'decoN');
 };
 
-const resetRadioButtons = () => {
-  for (const button of formRadioButtons) {
-    button.checked = false;
-  }
+const resetTextCard = () => {
+  defaultNameCard.innerHTML = 'Nombre Apellido';
+  defaultJobCard.innerHTML = 'Front-end developer';
 };
 
 const resetProfileImage = () => {
   uploadProfileImage.style.backgroundImage = '';
 };
 
-const resetTextCard = () => {
+// Resetear formulario
+
+const resetRadioButtons = () => {
+  for (const button of formRadioButtons) {
+    button.checked = false;
+  }
+};
+
+const resetInputsForm = () => {
   for (const textCard of formInformationUser) {
     textCard.value = textCard.defaultValue;
   }
-  defaultNameCard.innerHTML = 'Nombre Apellido';
-  defaultJobCard.innerHTML = 'Front-end developer';
+  inputEmail.value = '';
+  inputNumber.value = '';
+  inputLinkedin.value = '';
+  inputGit.value = '';
+};
+
+const resetCard = () => {
+  resetProfileImage();
+  resetTextCard();
+  resetCLassIcon();
+};
+const resetForm = () => {
+  resetRadioButtons();
+  resetInputsForm();
 };
 
 const handleReset = () => {
-  resetCLassIcon();
-  resetRadioButtons();
-  resetTextCard();
-  resetProfileImage();
+  resetCard();
+  resetForm();
 };
 
 resetButton.addEventListener('click', handleReset);
+
+// AÑADIR URLS A LOS ICONO - PASAR AL JS DE VALENTINA CUANDO LO VEAMOS TODAS PARA QUE ESTÉN LOS INPUTS LLAMADOS SOLO EN UN JS Y NO EN DOS
+
+function updateLinkContent() {
+  const iconEmail = document.querySelector('.js-linkEmail');
+  iconEmail.href = 'mailto:' + inputEmail.value;
+  const iconPhone = document.querySelector('.js-linkPhone');
+  iconPhone.href = 'tel:' + inputNumber.value;
+  const iconLiknedin = document.querySelector('.js-linkLinkedIn');
+  iconLiknedin.href = inputLinkedin.value;
+  const iconGit = document.querySelector('.js-linkGitHub');
+  iconGit.href = inputGit.value;
+}
+
+inputEmail.addEventListener('input', updateLinkContent);
+inputNumber.addEventListener('input', updateLinkContent);
+inputLinkedin.addEventListener('input', updateLinkContent);
+inputGit.addEventListener('input', updateLinkContent);
